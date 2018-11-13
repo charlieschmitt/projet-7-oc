e5075ea8fad74c0c359166431e2c6166d0e3a5ce
Schmitt <schmittcharlie@hotmail.fr>
2018-11-13 10:08:53 +0100

    ok comments

--- src/Components/GoogleMaps/Map.js
+++ src/Components/GoogleMaps/Map.js
@@ -5,33 +5,41 @@ class Map extends Component {
   
   constructor(props) {
     super(props)
+    // 
     this.state = {
+      // Données latitude - longitude
       currentLating: {
         lat: 0, 
         lng: 0
       }, 
+      // Marker map
       isMarkerShown: false
     }
   }
-
+  
+  // Obtenir la position exacte du user sur la map
   showCurrentLocation = () => {
+    // API geolocalisation
     if(navigator.geolocation){
+      // Récupérer les positions actuelles du user
       navigator.geolocation.getCurrentPosition(
         position => {
-          console.log(position.coords);
+          // Mis à jour des positions
           this.setState(prevState => ({
             currentLating: {
             ...prevState.currentLating,
             lat: position.coords.latitude,
             lng: position.coords.longitude
             },
+            // Passage à true du marker
             isMarkerShown: true
           }))
         }
       )
     }
   }
-
+  
+  // Activation des positions avant que l'app se monte
   componentDidMount() {
     this.showCurrentLocation()
   }
@@ -41,7 +49,9 @@ class Map extends Component {
     return(
       <Fragment>
         <MapWithAMarker
+          // Props Marker
           isMarkerShown={ this.state.isMarkerShown }
+          // Props Position
           currentLocation={ this.state.currentLating }
         />
       </Fragment>
--- src/Components/GoogleMaps/MapWithMarker.js
+++ src/Components/GoogleMaps/MapWithMarker.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
+// Recompose permet de créer des HOC (high-order components)
 import { compose, withProps } from 'recompose';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 const MapWithMarker = compose(
     withProps({
+        // API google maps
         googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCO_5DP0c2nkvFhOGG9EwyAUIo4ebiW2qA",
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div style={{ height: `400px` }} />,
@@ -13,9 +15,12 @@ const MapWithMarker = compose(
     withGoogleMap
 )((props) => 
     <GoogleMap
+        // Props zoom par défaut
         defaultZoom={ 12 }
+        // Props localisation actuelle
         center={{ lat: props.currentLocation.lat, lng: props.currentLocation.lng }}
     >
+        { /* Props Marker selon la position */ }
         { props.isMarkerShown && <Marker position={{ lat: props.currentLocation.lat, lng: props.currentLocation.lng }} onClick={ props.onMarkerClick } /> }
     </GoogleMap>
 )
